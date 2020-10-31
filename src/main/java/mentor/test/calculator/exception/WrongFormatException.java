e9d13fcc38fce0a47efede6cf3b1c22a08e72592
Viktor <53341437+Wiktor1-bot@users.noreply.github.com>
2020-10-31 13:27:08 +0300

    Update WrongFormatException.java

--- src/main/java/mentor/test/calculator/exception/WrongFormatException.java
+++ src/main/java/mentor/test/calculator/exception/WrongFormatException.java
@@ -3,6 +3,6 @@ package mentor.test.calculator.exception;
 public class WrongFormatException extends Exception{
 
     public WrongFormatException(){
-        System.err.println("Не правильный формат ввода цифр");
+        System.err.println("Incorrect number input format");
      }
 }
