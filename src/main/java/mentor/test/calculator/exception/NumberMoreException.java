e5bee90fa3045bae92823b785483fea8d73bf343
Viktor <53341437+Wiktor1-bot@users.noreply.github.com>
2020-10-31 13:24:06 +0300

    Update NumberMoreException.java

--- src/main/java/mentor/test/calculator/exception/NumberMoreException.java
+++ src/main/java/mentor/test/calculator/exception/NumberMoreException.java
@@ -6,6 +6,6 @@ public class NumberMoreException extends Exception{
     public static final String ANSI_GREEN = "\u001B[32m";
 
     public NumberMoreException(){
-        System.out.println(ANSI_GREEN+"Значение должно быть не больше 10"+ANSI_RESET);
+        System.out.println(ANSI_GREEN+"The value must be no more than 10"+ANSI_RESET);
     }
 }
